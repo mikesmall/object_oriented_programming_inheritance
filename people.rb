bdc66c2dc856825a2854076dc2b928f98e0ddbfe
mikesmall <michael.l.a.small@gmail.com>
2017-05-27 13:13:07 -0400

    Exercise 1 all done.

--- people.rb
+++ people.rb
@@ -30,4 +30,11 @@ cristina = Student.new('Cristina')
 
 # 8(a) Call the teach method on your instructor instance and call the learn method on your student.
 
+chris.teach
+
+cristina.learn
+
 # 8(b) Next, call the teach method on your student instance. What happens? Why doesn't that work? Leave a comment in your program explaining why.
+
+cristina.teach
+# This can't work because 'cristina' is an instance of the 'Student' class, and 'teach' is as instance-method belonging to the 'Instructor' class.
